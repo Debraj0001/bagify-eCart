d5da321368c4570e66224317a6b93f7abfa290d7
Debraj0001 <113236963+Debraj0001@users.noreply.github.com>
2024-12-21 19:21:53 +0530

    db changes

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -21,8 +21,8 @@ dotenv.config();
 const connectDB = async () => {
     try{
         await mongoose.connect(process.env.MONGODB_URI , {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
         })
         console.log('MongoDB connected');
         
@@ -33,5 +33,4 @@ const connectDB = async () => {
         
     }
 };
-
 module.exports = connectDB;
